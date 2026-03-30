91061751a9dfeb4badbdd0f6a08f57d0cd4c02fb
Tony-D4E <116191853+Tony-D4E@users.noreply.github.com>
2026-03-30 17:29:41 +0200

    main.tsx clean (retrait hydrateRoot)

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import { StrictMode } from 'react';
-import App from './App';
-import './index.css';
-import './styles/animations.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-const container = document.getElementById('root')!;
-const app = (
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
-
-if (container.hasChildNodes()) {
-  hydrateRoot(container, app);
-} else {
-  createRoot(container).render(app);
-}
+  </StrictMode>,
+)
