9e73caccc905f81d7d9618a2a45d1f966e3cda3d
Tony-D4E <116191853+Tony-D4E@users.noreply.github.com>
2026-04-01 08:32:53 +0200

    Footer: fix encodage + lien conditions-generales

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -74,7 +74,7 @@ const Footer: React.FC = () => {
               <img src="/picto-d4e.png" alt="D4E" style={{ width: 36, height: 36, borderRadius: 8 }} />
               <span className="ft-brand">Digital4Efficiency</span>
             </div>
-            <p className="ft-tagline">Partenaire Odoo Gold. IntÃ©grateur suisse depuis 2017.</p>
+            <p className="ft-tagline">Partenaire Odoo Gold. IntÃÂ©grateur suisse depuis 2017.</p>
             <div className="ft-contact-item">
               <svg className="ft-contact-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
               <a href="tel:+41582551115" className="ft-contact-link">+41 (0)58 255 11 15</a>
@@ -118,8 +118,8 @@ const Footer: React.FC = () => {
           </div>
 
           <div>
-            <div className="ft-heading">LÃ©gal</div>
-            <a className="ft-link" href="/conditions-generales">Conditions gÃ©nÃ©rales</a>
+            <div className="ft-heading">LÃÂ©gal</div>
+            <a className="ft-link" href="/conditions-generales">Conditions gÃÂ©nÃÂ©rales</a>
           </div>
         </div>
 
@@ -128,8 +128,8 @@ const Footer: React.FC = () => {
         <div className="ft-bottom">
           <div className="ft-bottom-left">
             <span>&copy; {new Date().getFullYear()} Digital4Efficiency</span>
-            <span>Â·</span>
-            <span><span className="ft-loc-dot" />GenÃ¨ve &amp; Sion</span>
+            <span>ÃÂ·</span>
+            <span><span className="ft-loc-dot" />GenÃÂ¨ve &amp; Sion</span>
           </div>
           <div className="ft-bottom-right">
             <span>Partenaire</span>
