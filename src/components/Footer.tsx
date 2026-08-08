b05ae115d280bb72b4d6130f400c161c274f09dc
Tony-D4E <116191853+Tony-D4E@users.noreply.github.com>
2026-08-08 14:49:36 +0200

    Add files via upload

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -105,6 +105,7 @@ const Footer: React.FC = () => {
           <div>
             <div className="ft-heading">Solutions Elvy</div>
             <a className="ft-link" href="/elvybat">ElvyBât</a>
+            <a className="ft-link" href="/elvyscan">ElvyScan</a>
             <a className="ft-link" href="/elvyprint">ElvyPrint</a>
             <a className="ft-link" href="/elvyinsurance">ElvyAssurance</a>
             <a className="ft-link" href="/elvyeduca">ElvyEduca</a>
