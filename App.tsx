dcbe1807de16d684c62bf8e80146b5dbb3bbef94
Tony-D4E <116191853+Tony-D4E@users.noreply.github.com>
2026-08-08 14:41:15 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,61 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navigation } from './sections/Navigation';
+import { WhatsAppWidget } from './components/WhatsAppWidget';
+import Footer from './components/Footer';
+import { Home } from './pages/Home';
+import { Elvy } from './pages/Elvy';
+import OdooPage from './pages/OdooPage';
+import ElvyBatPage from './pages/ElvyBatPage';
+import ElvyScanPage from './pages/ElvyScanPage';
+import ElvyPrintPage from './pages/ElvyPrintPage';
+import ElvyInsurancePage from './pages/ElvyInsurancePage';
+import ElvyEducaPage from './pages/ElvyEducaPage';
+import { TarifsPage } from './pages/TarifsPage';
+import { Support } from './pages/Support';
+import ConditionsPage from './pages/ConditionsPage';
+import EntreprisePage from './pages/EntreprisePage';
+import JobsPage from './pages/JobsPage';
+import { NotFound } from './pages/NotFound';
+
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <div className="min-h-screen bg-[#f8fafc]">
+        <Navigation />
+        <Routes>
+          <Route path="/"              element={<Home />} />
+          <Route path="/odoo"          element={<OdooPage />} />
+          <Route path="/elvy"          element={<Elvy />} />
+          <Route path="/elvybat"       element={<ElvyBatPage />} />
+          <Route path="/elvyscan"      element={<ElvyScanPage />} />
+          <Route path="/elvyprint"     element={<ElvyPrintPage />} />
+          <Route path="/elvyinsurance" element={<ElvyInsurancePage />} />
+          <Route path="/elvyeduca"     element={<ElvyEducaPage />} />
+          <Route path="/tarifs"        element={<TarifsPage />} />
+          <Route path="/support"       element={<Support />} />
+          <Route path="/conditions-generales"           element={<ConditionsPage />} />
+          <Route path="/entreprise"    element={<EntreprisePage />} />
+          <Route path="/jobs"          element={<JobsPage />} />
+          <Route path="*"              element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <WhatsAppWidget
+          phoneNumber="41764344595"
+          message="Bonjour Digital4Efficiency, j'ai une question concernant vos services Odoo."
+        />
+</div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
